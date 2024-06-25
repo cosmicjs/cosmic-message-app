835515237dab95eb80b624503c09375b0ebdc706
Tony Spiro <tspiro@tonyspiro.com>
2024-06-24 17:26:19 -0700

    edit: image link

--- cosmic/blocks/messages/Message.tsx
+++ cosmic/blocks/messages/Message.tsx
@@ -37,16 +37,21 @@ export const Message = ({ message }: { message: MessageType }) => {
       </div>
       {message.metadata?.image?.imgix_url && (
         <div className="mt-4">
-          <img
-            src={`${message.metadata?.image?.imgix_url}?w=600&auto=format,compression`}
-            alt={message.metadata?.image?.imgix_url}
-            className="h-[150px] w-[200px] rounded-xl object-cover"
-          />
+          <a
+            href={`${message.metadata?.image?.imgix_url}?w=600&auto=format,compression`}
+            target="_blank"
+          >
+            <img
+              src={`${message.metadata?.image?.imgix_url}?w=600&auto=format,compression`}
+              alt={message.metadata?.image?.imgix_url}
+              className="h-[150px] w-[200px] rounded-xl object-cover"
+            />
+          </a>
         </div>
       )}
       <div className="absolute right-0 bottom-0 p-4">
         {deleting ? (
-          <LoaderCircleIcon className="w-4 h-4 m-auto animate-spin" />
+          <LoaderCircleIcon className="text-red-500 w-4 h-4 m-auto animate-spin" />
         ) : (
           <TrashIcon
             className="text-red-500 w-4 h-4 cursor-pointer"
